5fc603cba3d9bcb4f3b018315dc811257d0bf372
andreaslarsen13 <andreas.larsen@zx-ventures.com>
2019-07-29 16:32:08 +0200

    added some basic css

--- popup.js
+++ popup.js
@@ -1,15 +1,43 @@
 StorageManager.retrieveAllText(function(items) {
 	for(var key of Object.keys(items)) {
-    var listNode = document.createElement("UL");
-    listNode.className = "listFormat"
+    var listNode = document.createElement("UL")
+    listNode.className += "project-item";
+    // listNode.className = "listFormat"
 
     var urlHeader = constructNodeWithText("H3", key);
+    var urlTitle = document.createElement('a');
 
-    listNode.appendChild(urlHeader);
+
+    var hostname;
+    //find & remove protocol (http, ftp, etc.) and get hostname
+
+    if (key.indexOf("//") > -1) {
+        hostname = key.split('/')[2];
+    }
+    else {
+        hostname = key.split('/')[0];
+    }
+
+    //find & remove port number
+    hostname = hostname.split(':')[0];
+    //find & remove "?,.com,.co"
+    hostname = hostname.split('?')[0];
+    hostname = hostname.split('.com')[0];
+    hostname = hostname.split('.co')[0];
+
+
+    var linkText = document.createTextNode(hostname);
+    urlTitle.appendChild(linkText);
+    urlTitle.href = key;
+    document.body.appendChild(urlTitle);
+
+    listNode.appendChild(urlTitle);
+
 
 		var item = items[key]
 		for(var text of item) {
       var listItem = constructNodeWithText("LI", text);
+      listItem.className += "project-item-title";
 
 			listNode.appendChild(listItem);
 		}
